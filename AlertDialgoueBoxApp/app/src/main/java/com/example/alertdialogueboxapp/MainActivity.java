fc159752e4fdd40cf4092b52ab0184c1ed3c375d
Khadeeja beevi <khadeejabeevinavas@gmail.com>
2024-09-14 20:01:47 +0530

    committed

---
+++ AlertDialgoueBoxApp/app/src/main/java/com/example/alertdialogueboxapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.alertdialogueboxapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.content.DialogInterface;
+import androidx.appcompat.app.AlertDialog;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void createAlert(View v){
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Do you want to Continue ?").setTitle("ALERT");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog dialog=builder.create();
+        builder.show();
+    }
+
+}
